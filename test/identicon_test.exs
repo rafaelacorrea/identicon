aac469c81543818556fd89a95724b65adab0ac4d
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2022-08-18 01:01:32 -0300

    test: :test_tube: creating main/1 test

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -1,3 +1,11 @@
 defmodule IdenticonTest do
   use ExUnit.Case
+  alias Identicon
+
+  describe "main/1" do
+    test "when a string value is passed, should return :ok" do
+      image = Identicon.main("Olá")
+      assert :ok == image
+    end
+  end
 end
