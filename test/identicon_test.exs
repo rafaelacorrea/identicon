579fa783905496f78f240bb02ef28a796ef6deeb
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2022-08-18 23:38:35 -0300

    test: :test_tube: test with arguent diferrent of string value

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -7,5 +7,12 @@ defmodule IdenticonTest do
       image = Identicon.main("Olá")
       assert :ok == image
     end
+
+    test "when a value different of string is passed, should return :error" do
+      image = Identicon.main({"Olá"})
+      {error, mensage} = image
+      assert :error == error
+      assert "insert a string valid value!" == mensage
+    end
   end
 end
